bd92231ec06b0cd875ab9d3578a771a0f1b29b0b
Iryna Petrah <webpetrash@gmail.com>
2023-09-21 17:16:56 -0400

    added some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
-import  {Header}  from './components/Header'
-import  {Loader}  from './components/Loader'
+import { Header } from './components/Header'
+import { Loader } from './components/Loader'
 import axios from 'axios'
 
-import  Map  from './components/map'
- import './App.css'
+import Map from './components/map'
+import './App.css'
 import { AddDisaster } from './components/addDisaster'
 
 function App() {
@@ -18,24 +18,24 @@ function App() {
       const response = await axios.get('https://eonet.gsfc.nasa.gov/api/v2.1/events?limit=5');
       const { events } = await response.data
 
-  //     setEventData(events)
-  //     setLoading(false)
-  //   }
+      //     setEventData(events)
+      //     setLoading(false)
+      //   }
 
-  //   fetchEvents()
-  // }, [])
-
+      //   fetchEvents()
+      // }, [])
+    }
+  })
 
   return (
     <>
       <Header />
-<<<<<<< HEAD
-      <AddDisaster />
-      {/* <Map /> */}
-      {/* { loading && <Map eventData={eventData}/> } */}
-=======
-     { loading && <Map eventData={eventData}/> }
->>>>>>> 5652b21a06e42898f673ad96dd0a65efbe7a757c
+      <div className="container">
+        <AddDisaster />
+        {/* <Map /> */}
+        {/* { loading && <Map eventData={eventData}/> } */}
+        {loading && <Map eventData={eventData} />}
+      </div>
     </>
   )
 }
